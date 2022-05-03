0e405cbf4af40d51bfb1fc0179d85a2133042f0e
Norbert Varga <varganorbert222u@gmail.com>
2022-05-03 23:30:48 +0200

    Initial MSSQL Migration added

--- Program.cs
+++ Program.cs
@@ -1,30 +1,29 @@
+//#define MSSQL
+//#define ORACLE
+#define POSTGRESQL
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ChatApp.Areas.Identity.Data;
-using System.Runtime.InteropServices;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-// {
-//     builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//         .AddEntityFrameworkStores<ChatAppIdentityOracleDbContext>();
-//     builder.Services.AddDbContext<ChatAppIdentityOracleDbContext>(options =>
-//         options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
-// }
-// else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-// {
-//     builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//         .AddEntityFrameworkStores<ChatAppIdentityPostgreDbContext>();
-//     builder.Services.AddDbContext<ChatAppIdentityPostgreDbContext>(options =>
-//         options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
-// }
-
+#if (MSSQL)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ChatAppIdentityMSSQLDbContext>();
+builder.Services.AddDbContext<ChatAppIdentityMSSQLDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLConnection")));
+#elif (POSTGRESQL)
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
         .AddEntityFrameworkStores<ChatAppIdentityPostgreDbContext>();
 builder.Services.AddDbContext<ChatAppIdentityPostgreDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
+#elif (ORACLE)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ChatAppIdentityOracleDbContext>();
+builder.Services.AddDbContext<ChatAppIdentityOracleDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
+#endif
 
 // Add services to the container.
 builder.Services.AddRazorPages();
