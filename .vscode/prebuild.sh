f2de8e2d0a76d656b16395960226918bf16dc41a
varganorbert222 <varganorbert222@gmail.com>
2022-05-07 19:24:39 +0200

    Fixing prebuild.sh

--- .vscode/prebuild.sh
+++ .vscode/prebuild.sh
@@ -2,4 +2,4 @@ fontDir="./wwwroot/fonts";
 if [ ! -d $fontDir ]; then
     mkdir -p $fontDir
 fi
-cp "./node_modules/bootstrap-icons/font/fonts/*" $fontDir
+cp ./node_modules/bootstrap-icons/font/fonts/* $fontDir
