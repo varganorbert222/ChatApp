88508b52cebb229adc8722ebbdec68d1084517e3
Norbert Varga <varganorbert222u@gmail.com>
2022-05-08 02:29:38 +0200

    Modified Error page

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error/GeneralError");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
